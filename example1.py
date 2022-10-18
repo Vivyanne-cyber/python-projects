6fd6f2a48550eaefb5387ba1a687447d1f45345e
Chisom Vivian Ogbu <113313643+Vivyanne-cyber@users.noreply.github.com>
2022-10-18 13:52:55 +0100

    Add files via upload

---
+++ example1.py
@@ -0,0 +1,6 @@
+name = input("What is your name?" + " ")
+age = input("What is your age?" + " ")
+height = input("What is your height?" + " ")
+print ("My name is" + " " + name + "," + " " + "I am", age, "years old" + " " + "with a height of", height)
+print (type(name))
+print (name.upper())
---
+++ example2.py
@@ -0,0 +1,4 @@
+firstname = "Vivian"
+lastname = "Ogbu"
+fullname = firstname + " " + lastname
+print (fullname)
