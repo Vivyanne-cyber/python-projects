2e51c5538d5b1ef7d8de1afa9ba6009f41845cb9
Chisom Vivian Ogbu <113313643+Vivyanne-cyber@users.noreply.github.com>
2022-10-24 10:48:07 +0100

    Add files via upload

---
+++ current_age_checker.py
@@ -0,0 +1,9 @@
+# To check the current age of the user
+age = int (input("Type age: "))
+date_of_birth = 2022 - age
+print (date_of_birth)
+
+# To check the user's year of birth
+year = int (input("What is your year of birth? "))
+current_age = 2022 - year
+print (current_age)
