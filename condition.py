fe47f5964aaf1a989439e81d10b849386d2bb395
Chisom Vivian Ogbu <113313643+Vivyanne-cyber@users.noreply.github.com>
2022-10-24 10:30:47 +0100

    Delete condition.py

--- condition.py
+++
@@ -1,8 +0,0 @@
-#check for the eligibility status of a user
-name = input("Hello" + "," + " " + "Welcome" + "," + " " + "kindly tell us your name?" + " ")
-print ("Nice to have you" +  " " + name + "," + "but before moving ahead" + "," "we would love to confirm your eligibility status")
-age = int (input("How old are you?" + " "))
-if age <= 30:
-    print ("Welcome onboard" + "," + "na your type we want")
-else:
-    print ("Nah!!" + "," + " " + "not allowed here" + ";" + " " + "check the next building for your spec")    
